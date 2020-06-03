57a9e9e7cd88c20df1916898830b6fd346ed0909
worldofdeveloper <developer.brest@gmail.com>
2020-06-03 12:32:15 +0300

    initial commit

---
+++ src/leds_stat.h
@@ -0,0 +1,5 @@
+
+void led(bool k){
+	if (k==true) digitalWrite(2, HIGH);
+	else digitalWrite(2,LOW);
+}
---
+++ src/mylib.h
@@ -0,0 +1,21 @@
+//Таймер Ильи
+// EXEC_TIMER_SET(время, действие;);
+#define COMBINE1(X,Y) X##Y  // helper macro
+#define COMBINE(X,Y) COMBINE1(X,Y)
+#define EXEC_TIMER_SET_NM(Tname,Tafter,Tcode) \
+{                                             \
+  static uint32_t Tname = millis()+Tafter;    \
+  if(Tname < millis()){                       \
+    Tname = millis()+Tafter;                  \
+    {Tcode}                                   \
+  }                                           \
+}
+#define EXEC_TIMER_SET(Tafter,Tcode) EXEC_TIMER_SET_NM(COMBINE(__FUNCTION__,__LINE__),Tafter,Tcode)
+
+
+//Конвертнуть из строки в бул
+bool toBool(String s){
+  if(s.equals("true")||s.equals("1")||s.equals("TRUE"))return true;
+  if(s.equals("false")||s.equals("0")||s.equals("FALSE"))return false;
+  return false;
+}
